46d78109c19659d39a56173312547b6d361bab75
Taha <taha100_100@yahoo.com>
2024-05-29 14:31:47 +0300

    Add project files

---
+++ DVLD_Business/clsUser.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using DVLD_DataAccess;
+
+namespace DVLD_Buisness
+{
+    public class clsUser
+    {
+        private string _Password;
+        public enum enMode { AddNew = 0, Update = 1 };
+        public enMode Mode = enMode.AddNew;
+
+        public int UserID { set; get; }
+        public int PersonID { set; get; }
+        public clsPerson PersonInfo;
+        public string UserName { set; get; }
+        public string Password {
+            set
+            { 
+                _Password = value; 
+            }
+            get 
+            {
+                return _Password; 
+            }
+        }
+        public bool IsActive { set; get; }
+
+        public clsUser()
+
+        {
+            this.UserID = -1;
+            this.UserName = "";
+            this.Password = "";
+            this.IsActive = true;
+            Mode = enMode.AddNew;
+            
+
+        }
+        public static bool UpdateAllPasswords()
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+
+
+                using (SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    connection.Open();
+                    string query = @"Select * from Users";
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.HasRows)
+
+                            {
+                                dt.Load(reader);
+                            }
+                        }
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+
+
+                clsUserData.ChangePassword((int)dt.Rows[i]["UserID"], HashPasswordUsingSHA256(dt.Rows[i]["Password"].ToString()));
+            }
+
+            return true;
+        }
+        private clsUser(int UserID, int PersonID, string Username, string Password,
+            bool IsActive)
+
+        {
+            this.UserID = UserID;
+            this.PersonID = PersonID;
+            this.PersonInfo = clsPerson.Find(PersonID);
+            this.UserName = Username;
+            this.Password = Password;
+            this.IsActive = IsActive;
+
+            Mode = enMode.Update;
+        }
+
+        private bool _AddNewUser()
+        {
+
+            this.UserID = clsUserData.AddNewUser(this.PersonID, this.UserName,
+               HashPasswordUsingSHA256(this.Password), this.IsActive);
+
+            return (this.UserID != -1);
+        }
+        private bool _UpdateUser()
+        {
+
+            return clsUserData.UpdateUser(this.UserID, this.PersonID, this.UserName,
+                HashPasswordUsingSHA256(this.Password), this.IsActive);
+        }
+
+        public static string HashPasswordUsingSHA256(string Password)
+        {
+            using (SHA256 sh = SHA256.Create())
+            {
+                byte[] hashBytes = sh.ComputeHash(Encoding.UTF8.GetBytes(Password));
+                return BitConverter.ToString(hashBytes).Replace("-", "");
+            }
+        }
+        public static clsUser FindByUserID(int UserID)
+        {
+            int PersonID = -1;
+            string UserName = "", Password = "";
+            bool IsActive = false;
+
+            bool IsFound = clsUserData.GetUserInfoByUserID
+                                (UserID, ref PersonID, ref UserName, ref Password, ref IsActive);
+
+            if (IsFound)
+
+                return new clsUser(UserID, PersonID, UserName, Password, IsActive);
+            else
+                return null;
+        }
+        public static clsUser FindByPersonID(int PersonID)
+        {
+            int UserID = -1;
+            string UserName = "", Password = "";
+            bool IsActive = false;
+
+            bool IsFound = clsUserData.GetUserInfoByPersonID
+                                (PersonID, ref UserID, ref UserName, ref Password, ref IsActive);
+
+            if (IsFound)
+
+                return new clsUser(UserID, PersonID, UserName, Password, IsActive);
+            else
+                return null;
+        }
+        public static clsUser FindByUsernameAndPassword(string UserName, string Password)
+        {
+            int UserID = -1;
+            int PersonID = -1;
+
+            bool IsActive = false;
+
+            bool IsFound = clsUserData.GetUserInfoByUsernameAndPassword
+                                (UserName,HashPasswordUsingSHA256(Password), ref UserID, ref PersonID, ref IsActive);
+
+
+            if (IsFound)
+
+                return new clsUser(UserID, PersonID, UserName, Password, IsActive);
+            else
+                return null;
+        }
+
+        public bool Save()
+        {
+            switch (Mode)
+            {
+                case enMode.AddNew:
+                    if (_AddNewUser())
+                    {
+
+                        Mode = enMode.Update;
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                case enMode.Update:
+
+                    return _UpdateUser();
+
+            }
+
+            return false;
+        }
+
+        public static DataTable GetAllUsers()
+        {
+            return clsUserData.GetAllUsers();
+        }
+
+        public static bool DeleteUser(int UserID)
+        {
+            return clsUserData.DeleteUser(UserID);
+        }
+
+        public static bool isUserExist(int UserID)
+        {
+            return clsUserData.IsUserExist(UserID);
+        }
+
+        public static bool isUserExist(string UserName)
+        {
+            return clsUserData.IsUserExist(UserName);
+        }
+
+        public static bool isUserExistForPersonID(int PersonID)
+        {
+            return clsUserData.IsUserExistForPersonID(PersonID);
+        }
+
+        public bool ChangePassword()
+        {
+            return clsUserData.ChangePassword(this.UserID,this.Password);
+        }
+
+
+    }
+}
---
+++ DVLD_DataAccess/clsDataAccessSettings.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Configuration;
+
+namespace DVLD_DataAccess
+{
+    public static class clsDataAccessSettings
+    {
+        public static string ConnectionString = ConfigurationManager.ConnectionStrings["MyDBConnectionString"].ConnectionString;
+    }
+}
